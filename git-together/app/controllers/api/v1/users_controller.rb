ee64a2276ebd012b75c0a68dd1237285651563f4
ioreka <lucymhodges@gmail.com>
2018-08-20 13:03:20 +0100

    authentication & authorisation. requires debugging

--- git-together/app/controllers/api/v1/users_controller.rb
+++ git-together/app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,52 @@
 class Api::V1::UsersController < ApplicationController
+
+  def create
+    user = User.create(username: params[:username], password: params[:password])
+    if user.valid?
+      render json: { token: issue_token({ id: user.id }) }
+    else
+      render json: { error: 'Cannot create user!' }
+    end
+  end
+
+  def login
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      render json: { token: issue_token({ id: user.id }) }
+    else
+      render json: { error: 'Cannot find or authenticate user.' }
+    end
+  end
+
+  def get_current_user
+    if current_user
+      render json: { username: current_user.username, id: current_user.id }
+    else
+      render json: { error: 'no user' }
+    end
+  end
+
+  def get_events
+    if current_user && params[:id].to_i == current_user.id
+      my_events = current_user.events
+      render json: my_events
+    else
+      render json: { error: 'Not authorized!' }
+    end
+  end
+
+  def set_events
+    if current_user && params[:id].to_i == current_user.id
+      # current_user.event_ids = params[:event_ids].map do |id|
+      #   if id == 129
+      #     130
+      #   else
+      #     id
+      #   end
+      # end
+      render json: current_user.events
+    else
+      render json: { error: 'Not authorized!' }
+    end
+  end
 end
--- git-together/app/models/user.rb
+++ git-together/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :userevents
   has_many :comments
   has_many :events, through: :userevents
+
+  has_secure_password
 end
